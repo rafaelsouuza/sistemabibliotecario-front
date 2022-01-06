41001e5f65adf5162c1deae4646447628e29ce11
= <=>
2022-01-05 23:31:55 -0300

    HTML e CSS para USUARIO-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
-      { path: 'usuarios', component: UsuarioListComponent}
+      { path: 'usuarios', component: UsuarioListComponent }
     ]
   }
 ];
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+    id?: any;
+    nome: string;
+    email: string;
+    senha: string;
+    perfis: string[];
+    dataCriacao: any;
+}
