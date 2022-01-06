dc2c8dc804966d061af16a3321a460a57a6a87b6
= <=>
2022-01-06 17:03:56 -0300

    Interceptor para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -75,7 +76,7 @@ import { ToastrModule } from 'ngx-toastr';
        progressBar: true
      })
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
