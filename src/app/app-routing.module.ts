6e1a7fbe00b9ca30b24b131f83042186fd998680
= <=>
2022-01-06 01:34:58 -0300

    Autenticação de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -8,7 +9,7 @@ import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
-    path: '', component: NavComponent, children: [
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
       { path: 'usuarios', component: UsuarioListComponent }
     ]
