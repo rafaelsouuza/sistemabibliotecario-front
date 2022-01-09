a9c979f3a6a9e4b3f7f38e89ca6ad0a7266c0555
= <=>
2022-01-09 01:02:49 -0300

    Botões de ação para usuário

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -16,7 +16,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['usuarios/create'])
+    this.router.navigate(['usuarios'])
   }
 
   logout() {
