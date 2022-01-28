0c43a5d2344235c7442e86b2c5311cf0c05a7c01
= <=>
2022-01-27 23:49:53 -0300

    Componente EMPRESTIMO-READ finalizado

--- src/app/components/emprestimo/emprestimo-read/emprestimo-read.component.ts
+++ src/app/components/emprestimo/emprestimo-read/emprestimo-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Emprestimo } from 'src/app/models/emprestimo';
+import { EmprestimoService } from 'src/app/services/emprestimo.service';
 
 @Component({
   selector: 'app-emprestimo-read',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmprestimoReadComponent implements OnInit {
 
-  constructor() { }
+  emprestimo: Emprestimo = {
+    status: '',
+    cliente: '',
+    livro: '',
+    nomeCliente: '',
+    nomeLivro: '',
+  }
+
+  constructor(
+    private emprestimoService: EmprestimoService,
+    private toastService: ToastrService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.emprestimo.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
+
+  findById(): void {
+    this.emprestimoService.findById(this.emprestimo.id).subscribe(resposta => {
+      this.emprestimo = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): String {
+    if(status == '0') {
+      return 'EMPRESTADO'
+    } else if(status == '1') {
+      return 'DEVOLVIDO'
+    } else {
+      return 'ATRASADO'
+    }
+   }
 
 }
